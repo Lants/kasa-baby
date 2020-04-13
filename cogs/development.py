b61dd034a71abd52abd9ea8183ca1e4a9b7b8f85
Lants <lchongki@ucsc.edu>
2020-04-12 19:11:06 -0700

    Set up Development cog

---
+++ cogs/development.py
@@ -0,0 +1,49 @@
+# Developer commands and tools for kasa baby bot
+
+from discord.ext import commands
+import random
+
+class Development(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+    #------------------------------------------COMMANDS------------------------------------------
+
+    # Close connection to Discord
+    @commands.command(name = "sleep", description = "Put me to sleep!")
+    @commands.check_any(commands.has_permissions(administrator = True), commands.is_owner())
+    async def sleep(self, ctx):
+        if random.random() < .5:
+            await ctx.channel.send("nighty night! I go sleep now " + str(ctx.author.mention))
+        else:
+            await ctx.channel.send("aw, it's not my bedtime yet " + str(ctx.author.mention) + "! fine.. goodnight!")
+        await ctx.bot.logout()
+        
+    # Echo back input. Used for testing.
+    @commands.command(name = "echo", description = "Echoes back input. Used for testing.")
+    @commands.check_any(commands.has_permissions(administrator = True), commands.is_owner())
+    async def echo(self, ctx, *, message: str):
+        await ctx.send(message)
+
+
+
+
+
+    #-------------------------------------------COMMAND ERROR HANDLING-----------------------------------------------    
+    @echo.error
+    async def echo_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("uhh.. ur not good enough to do this sorry")
+        
+    @sleep.error    
+    async def sleep_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("Only my parents can make make me go to bed! (Admins only dummy)")
+        
+        
+        
+def setup(bot):
+    bot.add_cog(Development(bot))        
+        
+        
--- rewrite.py
+++ rewrite.py
@@ -13,68 +13,21 @@ description = '''Discord bot for KASA server. Imitates a baby. googoo gaga, bich
 
 bot = commands.Bot(command_prefix = '!', description = description, case_insensitive = True, owner_id = lance)
 
-
+#-------------Cogs-------------
+# Cogs are an organizational system
+cogs = ['cogs.development']
 
 
 # On bot launch
 @bot.event
 async def on_ready():
     print('Logged in as ' + bot.user.name)
-    
-# On bot received message
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    
-    print('received: ' + message.content)
+    for cog in cogs:
+        bot.load_extension(cog)
+    return
     
     
-    
-    # Run any commands that were called
-    await bot.process_commands(message)
-    
-    
-#------------------------------------------COMMANDS------------------------------------------
 
-# Close connection to Discord
-# note: ctx is Context, see Discord.py docs for more details.
-@bot.command()
-@commands.check_any(commands.has_permissions(administrator = True), commands.is_owner())
-async def sleep(ctx):
-    if random.random() < .5:
-        await ctx.channel.send("nighty night! I go sleep now " + str(ctx.author.mention))
-    else:
-        await ctx.channel.send("aw, it's not my bedtime yet " + str(ctx.author.mention) + "! fine.. goodnight!")
-    await ctx.bot.logout()
-    
-# Echo back input. Used for testing.
-@bot.command()
-@commands.check_any(commands.has_permissions(administrator = True), commands.is_owner())
-async def echo(ctx, *, message: str):
-    await ctx.send(message)
-
-
-
-
-
-
-
-#-------------------------------------------COMMAND ERROR HANDLING-----------------------------------------------    
-@echo.error
-async def echo_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("uhh.. ur not good enough to do this sorry")
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 #---------------------------------------IGNORE-------------------------------------------------
 
 
